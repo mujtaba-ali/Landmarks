7718ce2088e53d4dca4affea230799295f170be3
Mohammed Mujtaba Ali <mujtabaali@Mujtabas-MacBook-Pro.local>
2022-10-30 02:36:14 +0530

    first commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Mohammed Mujtaba Ali on 30/10/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock").clipShape(Circle()).overlay {
+            Circle().stroke(.white, lineWidth: 4)
+        }
+        .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mohammed Mujtaba Ali on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
