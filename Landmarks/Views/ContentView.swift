0101d45085d465987305a29de993dfa5918dacb6
Mohammed Mujtaba Ali <mujtabaali@Mujtabas-MacBook-Pro.local>
2022-10-30 16:41:57 +0530

    Adding Nav,Views,Models

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Mohammed Mujtaba Ali on 30/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
